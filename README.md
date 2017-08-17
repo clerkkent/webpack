a2f92d41680e2297f7673c63e9786f700bf597a4
shenqingqing <523970301@qq.com>
2017-08-17 16:18:22 +0800

    webpack 开发环境配置demo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # webpack
 webpack入门
+##运行项目需安装以下文件
+node.js安装 https://nodejs.org/en/download/ 
+ //全局安装webpack
+npm install -g webpack
+//局部安装到你的项目目录
+npm install --save-dev webpack
+ 
+
+安装完毕后执行 npm run dev  
+打开浏览器访问 http://localhost:8080  
+
---
+++ app/Greeter.js
@@ -0,0 +1,20 @@
+// Greeter.js
+// var config=require('./config.json');
+// module.exports = function() {
+//     var greet = document.createElement('div');
+//     greet.textContent = config.greetText;
+//     return greet;
+// };
+import React,{Component} from 'react'
+// import config from './config,json';
+import styles from './Greeter.css' //导入
+class Greeter extends Component{
+	render(){
+		return(
+			<div className={styles.root}>
+				hi shenqingqing!!!
+			</div>
+			);
+	}
+}
+export default Greeter
